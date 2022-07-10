b68b0c5ca9cc55cd68bb29ea8d25ad29c171da7f
JordanPavlev <danipavlev@gmail.com>
2022-07-10 16:15:30 +0300

    new style TODO responsive

--- main.js
+++ main.js
@@ -1,69 +1,54 @@
-const secret_api ='at_qIuBqv8fScvDhcIgRZ8ZrhVvOg9Cd'
-const bypass_cors_url ='http(s)://thingproxy.freeboard.io/fetch/'
-const api_uri ='https://geo.ipify.org/api'
-let current_verion ='v2'
+var ipDisplay = document.getElementById("current_ip");
+var locationDisplay = document.getElementById("current_town");
+var timezoneDisplay = document.getElementById("current_zone");
+var ispDisplay = document.getElementById("current_isp");
+var mapDisplay = document.getElementById("display_map");
 
-let currentIp = document.getElementById('current_ip');
-let currentTown = document.getElementById('current_town');
-let currentZone = document.getElementById('current_zone');
-let currentISP = document.getElementById('current_isp');
-const enteredIP = document.getElementById('ip_address');
-const searchBtn = document.getElementById('search_btn');
-
-const headersOption = {
-    headers: {
-        'Acces-Control-Allow-Origin': '*',
-        'dataType': 'json',
-    }
-}
-
-const map = L.map('display_map', {
-    'center': [0,0],
-    'zoom': 0,
-    'layers': [
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          })
-    ]
-})
+var mymap = L.map('display_map').setView([34.0614, -118.08162], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoia2V2ZXRpaDg2MSIsImEiOiJja2h4MzFxaG8wOW5pMzBsdGZ1NXFoeHh5In0.hw5mLyF4KWalDgcxAWrmuw'
+}).addTo(mymap);
+var marker = L.marker([34.0614, -118.08162]).addTo(mymap);
 
 
-updateMarker = (update_marker = [-33.665, 18.993]) => {
-    map.setView(update_marker, 13);
-    L.marker(update_marker).addTo(map);
-}
+function main(ipAddress){
+  var ip = ipAddress;
+  var api_key = 'at_qIuBqv8fScvDhcIgRZ8ZrhVvOg9Cd';
+  var api_url = 'https://geo.ipify.org/api/v1?';
+  var url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip;
 
-getIPDetails = (default_ip) => {
-    if(default_ip == undefined){
-        var ip_url = `${bypass_cors_url}${api_uri}${current_verion}?apiKey=${secret_api}`
-    }
-    else {
-        var ip_url = `${bypass_cors_url}${api_uri}${current_verion}?apiKey=${secret_api}&ipAddress=${default_ip}`
-    }
-    fetch(ip_url, headersOption)
-    .then( results => results.json())
-    .then( data => {
-        currentIp.innerHTML = data.ip
-        currentTown.innerHTML = `${data.location.city} ${data.location.country} ${data.location.postalCode}`
-        currentZone.innerHTML = data.location.timezone
-        currentISP.innerHTML = data.isp 
-
-        // update map marker 
-        updateMarker([data.location.lat, data.location.lng])
+  
+  fetch(url)
+    .then(res => {
+      return res.json();
     })
-    .catch(error => {
-        alert("Unable to get IP details")
-        console.log(error)
+    .then(res => {
+      findInfo(res);
+      renderMap(res);
     })
 }
 
-document.addEventListener('load', updateMarker())
+function findInfo(res){
+  console.log(res);
+  ipDisplay.innerText = res.ip;
+  locationDisplay.innerText = res.location.region + ", " + res.location.country + " ";
+  timezoneDisplay.innerText = "UTC " + res.location.timezone;
+  ispDisplay.innerText = res.isp;  
+}
 
-searchBtn.addEventListener('click', e => {
-    e.preventDefault()
-    if (enteredIP.value != '' && enteredIP.value != null) {
-        getIPDetails(enteredIP.value)
-        return
-    }
-    alert("Please enter a valid IP address");
+function renderMap(res){
+  mymap.setView([res.location.lat, res.location.lng], 13);
+  marker.setLatLng([res.location.lat, res.location.lng])
+}
+
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  var userInput = e.target[0].value;
+  // add an ip validation here
+  main(userInput);
 })
